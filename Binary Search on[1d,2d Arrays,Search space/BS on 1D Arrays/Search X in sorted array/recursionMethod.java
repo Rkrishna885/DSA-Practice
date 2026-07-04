53d6209204f768ed1c532ec9978a14c8dcf893a4
krishna <rentikotaramaKrishna86@gmail.com>
2026-07-04 09:39:51 +0530

    added binary search method using iteration and recursion methods.

---
+++ Binary Search on[1d,2d Arrays,Search space/BS on 1D Arrays/Search X in sorted array/recursionMethod.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int bs(int[] arr,int low,int high,int target){
+        if(low>high){
+            return -1;
+        }
+        int mid=(low+high)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+           return bs(arr,low,mid-1,target);
+        }
+        return bs(arr,mid+1,high,target);
+    }
+    public int search(int[] nums, int target) {
+       return bs(nums,0,nums.length-1,target);
+    }
+}
