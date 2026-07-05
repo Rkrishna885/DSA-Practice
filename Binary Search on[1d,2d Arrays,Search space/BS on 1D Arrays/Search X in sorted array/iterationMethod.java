6b0a60bce62bb123d61780e7c0b90334cca5a4ac
krishna <rentikotaramaKrishna86@gmail.com>
2026-07-05 08:47:30 +0530

    added upperBound and LowerBound of x problem and Search insert position problem with code.

--- Binary Search on[1d,2d Arrays,Search space/BS on 1D Arrays/Search X in sorted array/iterationMethod.java
+++ Binary Search on[1d,2d Arrays,Search space/BS on 1D Arrays/Search X in sorted array/iterationMethod.java
@@ -1,3 +1,22 @@
+// Search X in sorted array
+// Given a sorted array of integers nums with 0-based indexing, find the index of a specified target integer. If the target is found in the array, return its index. If the target is not found, return -1.
+
+
+// Example 1
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+
+// Output: 4
+
+// Explanation: The target integer 9 exists in nums and its index is 4
+
+// Example 2
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+
+// Output: -1
+
+// Explanation: The target integer 2 does not exist in nums so return -1
 class Solution {
     public int search(int[] nums, int target) {
        int low=0;
